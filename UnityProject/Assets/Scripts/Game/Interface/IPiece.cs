7af9632693fb9ff75d055b18349c837e5f88e40f
Nico <n.jesson@exkee.com>
2018-04-26 11:56:26 +0200

    [code] added a few comments on interfaces

--- UnityProject/Assets/Scripts/Game/Interface/IPiece.cs
+++ UnityProject/Assets/Scripts/Game/Interface/IPiece.cs
@@ -5,6 +5,8 @@
 /// </summary>
 public interface IPiece
 {
+    /// <summary>The Id of the owner team</summary>
     int TeamId { get; }
+    /// <summary>The current world position of the Piece</summary>
     Vector3 Position { get; }
 }
