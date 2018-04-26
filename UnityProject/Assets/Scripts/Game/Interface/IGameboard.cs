27a270b5653bc60da43225040d284cceacfe5010
Nico <n.jesson@exkee.com>
2018-04-26 12:07:51 +0200

    [code] added a few more comments on interfaces

--- UnityProject/Assets/Scripts/Game/Interface/IGameboard.cs
+++ UnityProject/Assets/Scripts/Game/Interface/IGameboard.cs
@@ -3,11 +3,15 @@
 /// </summary>
 public interface IGameboard
 {
+    /// <summary>
+    /// The Id of the Neutral Homes (-1)
+    /// </summary>
     int NeutralTeamId { get; }
 
     /// <summary>
     /// Returns all the existing Homes
     /// </summary>
+    /// <remarks>You can trust this buffer over time</remarks>
     IHome[] Homes { get; }
 
     /// <summary>
@@ -16,10 +20,11 @@ public interface IGameboard
     /// <param name="teamId"></param>
     /// <param name="belongToTeam">Says if we want the Home of our Team or the ones from others</param>
     /// <returns></returns>
+    /// <remarks>You can not trust this buffer over time, IHomes may see their Id change</remarks>
     IHome[] GetHomes(int teamId, bool belongToTeam);
 
     /// <summary>
-    /// 
+    /// Those are the Boldies alive and visible on the map (not the ones in IHomes)
     /// </summary>
     /// <param name="teamId"></param>
     /// <returns></returns>
